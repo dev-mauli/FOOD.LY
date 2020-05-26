70054b64f9f29e5b90eac0802686c38e684c1274
sahilmistry08 <63896840+sahilmistry08@users.noreply.github.com>
2020-05-26 22:13:32 +0530

    profile

--- FOOD.LY/Controllers/ProfileController.cs
+++ FOOD.LY/Controllers/ProfileController.cs
@@ -38,5 +38,30 @@ namespace FOOD.LY.Controllers
             string m_x_Result = ClassConverterHelper.ConnectToAPI(api);
             return Json(new { msg = m_x_Result }, JsonRequestBehavior.AllowGet);
         }
+
+
+        [HttpPost]
+        public ActionResult ProfileDetailsUpdate(string mdl)
+        {
+            try
+            {
+                T_LOGIN_BE __mdl = serializer.Deserialize<T_LOGIN_BE>(mdl);
+                __mdl.ID = Convert.ToInt32(Session[SessionKeys.LOGINID]);
+
+                T_TOKEN_BE api = new T_TOKEN_BE
+                {
+                    TOKENPATH = "/api/Profile/ProfileUPDATEdetails",
+                    TOKEMSG = serializer.Serialize(__mdl),
+                    ENTEREDBY = Convert.ToInt32(Session[SessionKeys.LOGINID])
+                };
+
+                string m_x_Result = ClassConverterHelper.ConnectToAPI(api);
+                return Json(new { msg = m_x_Result }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
--- FOOD.LY/Scripts/Profile.js
+++ FOOD.LY/Scripts/Profile.js
@@ -1,41 +1,136 @@
 ﻿$(document).ready(function () {
-    AllPost();
-    function AllPost() {
-        var url = '/Profile/Profiledetails';
-		$.ajax({
-			url: url,
-			type: "POST",
-			processData: false,
-			contentType: false,
-			data: {},
-			success: function (data) {
-                var newData = $.parseJSON(data.msg);
-                {
-                    //FOR LABEL SHOW
-                    $('#txtFULLNAMElbl').append(newData[0].FULLNAME);
-                    $('#txtEMAIL1lbl').append(newData[0].EMAIL);
-                    $('#txtCONTACTNUMlbl').append(newData[0].MOBILENO);
-                    $('#txtINSTALINKlbl').append(newData[0].INSTALINK);
-                    $('#txtYOUTUBELINKlbl').append(newData[0].YOUTUBELINK);
-                    $('#txtFACEBOOKLINKlbl').append(newData[0].FBLINK);
+    ProfileData();
 
-                    //FOR PROFILE NAME
-                    $('#lblprofilename').append(newData[0].FULLNAME);
-
-                    //FOR EDIT
-                    $('#txtFULLNAME_EDIT').val(newData[0].FULLNAME);
-                    $('#txtEMAIL1_EDIT').val(newData[0].EMAIL);
-                    $('#txtCONTACTNUM_EDIT').val(newData[0].MOBILENO);
-                    $('#txtINSTALINK_EDIT').val(newData[0].INSTALINK);
-                    $('#txtYOUTUBELINK_EDIT').val(newData[0].YOUTUBELINK);
-                    $('#txtFACEBOOKLINK_EDIT').val(newData[0].FBLINK);
-
-                }
-                
-			},
-			error: function (er) {
-
-			}
-		});
-	}
 });
+
+//Load profile data
+function ProfileData() {
+    var url = '/Profile/Profiledetails';
+    $.ajax({
+        url: url,
+        type: "POST",
+        processData: false,
+        contentType: false,
+        data: {},
+        success: function (data) {
+            var newData = $.parseJSON(data.msg);
+            {
+                //FOR LABEL SHOW
+                $('#txtFULLNAMElbl').html(newData[0].FULLNAME);
+                $('#txtEMAIL1lbl').html(newData[0].EMAIL);
+                $('#txtCONTACTNUMlbl').html(newData[0].MOBILENO);
+                $('#txtINSTALINKlbl').html(newData[0].INSTALINK);
+                $('#txtYOUTUBELINKlbl').html(newData[0].YOUTUBELINK);
+                $('#txtFACEBOOKLINKlbl').html(newData[0].FBLINK);
+
+                //FOR PROFILE NAME
+                $('#lblprofilename').append(newData[0].FULLNAME);
+
+                //FOR EDIT
+                $('#txtID_EDIT').val(newData[0].ID);
+                $('#txtFULLNAME_EDIT').val(newData[0].FULLNAME);
+                $('#txtEMAIL1_EDIT').val(newData[0].EMAIL);
+                $('#txtCONTACTNUM_EDIT').val(newData[0].MOBILENO);
+                $('#txtINSTALINK_EDIT').val(newData[0].INSTALINK);
+                $('#txtYOUTUBELINK_EDIT').val(newData[0].YOUTUBELINK);
+                $('#txtFACEBOOKLINK_EDIT').val(newData[0].FBLINK);
+
+            }
+
+        },
+        error: function (er) {
+
+        }
+    });
+}
+
+//Update profile
+function UpdateProf() {
+    var REGISTER_MST_UPDT =
+    {
+        FULLNAME: $('#txtFULLNAME_EDIT').val(),
+        MOBILENO: $('#txtCONTACTNUM_EDIT').val(),
+        INSTALINK: $('#txtINSTALINK_EDIT').val(),
+        FBLINK: $('#txtFACEBOOKLINK_EDIT').val(),
+        YOUTUBELINK: $('#txtYOUTUBELINK_EDIT').val()
+    };
+
+    var REGISTER_MST_JSON = JSON.stringify(REGISTER_MST_UPDT);
+
+    var url = '/Profile/ProfileDetailsUpdate';
+
+    if ($("#btnregisterUpdate").val() === "UpdateProf") {
+        if (!isvalidProfile()) {
+            Swal.fire({ title: "Please Enter Values!", confirmButtonColor: "#3051d3", icon: "error" });
+        }
+        else {
+            $.ajax({
+                url: url,
+                data: { mdl: REGISTER_MST_JSON },
+                dataType: 'json',
+                type: 'POST',
+                success: function (data) {
+                    ProfileData();
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: 'Seved Successfully.',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                },
+                error: function (data) {
+                    Swal.fire({ title: "Something Went Wrong", text: "Reason:" + data, confirmButtonColor: "#3051d3", icon: "error" });
+                    return false;
+                }
+
+            });
+        }
+    }
+
+}
+
+//Profile Validation
+function isvalidProfile() {
+    var rntValue = true;
+
+    if ($('#txtFULLNAME_EDIT').val() === '') {
+        $('#txtFULLNAME_EDIT').css('border-bottom', '1px solid red');
+        rntValue = false;
+    }
+    else {
+        $('#txtFULLNAME_EDIT').css('border-bottom', '1px solid #ccc');
+    }
+   
+    if ($('#txtCONTACTNUM_EDIT').val() === '') {
+        $('#txtCONTACTNUM_EDIT').css('border-bottom', '1px solid red');
+        rntValue = false;
+    }
+    else {
+        $('#txtCONTACTNUM_EDIT').css('border-bottom', '1px solid #ccc');
+    }
+    if ($('#txtINSTALINK_EDIT').val() === '') {
+        $('#txtINSTALINK_EDIT').css('border-bottom', '1px solid red');
+        rntValue = false;
+    }
+    else {
+        $('#txtINSTALINK_EDIT').css('border-bottom', '1px solid #ccc');
+    }
+
+    if ($('#txtYOUTUBELINK_EDIT').val() === '') {
+        $('#txtYOUTUBELINK_EDIT').css('border-bottom', '1px solid red');
+        rntValue = false;
+    }
+    else {
+        $('#txtYOUTUBELINK_EDIT').css('border-bottom', '1px solid #ccc');
+    }
+    if ($('#txtFACEBOOKLINK_EDIT').val() === '') {
+        $('#txtFACEBOOKLINK_EDIT').css('border-bottom', '1px solid red');
+        rntValue = false;
+    }
+    else {
+        $('#txtFACEBOOKLINK_EDIT').css('border-bottom', '1px solid #ccc');
+    }
+
+    return rntValue;
+}
